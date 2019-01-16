7fe0c98aabe75a18f587921720d32dae120b6a3d
Olga Albisser <olga@albisser.org>
2019-01-16 15:22:28 +0100

    tc: add dualpi2 scheduler module
    DUALPI2 AQM is a combination of the DUALQ Coupled-AQM with a PI2 base-AQM,
    able to control scalable congestion controls like DCTCP and TCP-Prague.
    The PI2 AQM details can be found in the paper:
    CoNEXT '16 Proceedings of the 12th International on Conference on emerging
    Networking EXperiments and Technologies : "PI2: A Linearized AQM for both
    Classic and Scalable TCP".
    PI2 makes quite some PIE heuristics unnecessary, allowing to use both
    Reno/Cubic in parallel with DCTCP, maintaining window fairness.
    
    Additional details can be found in the draft:
    https://www.ietf.org/id/draft-ietf-tsvwg-aqm-dualq-coupled
    
    Signed-off-by: Olga Albisser <olga@albisser.org>
    Signed-off-by: Koen De Schepper <koen.de_schepper@nokia-bell-labs.com>
    Signed-off-by: Oliver Tilmans <olivier.tilmans@nokia-bell-labs.com>
    Signed-off-by: Bob Briscoe <research@bobbriscoe.net>
    Signed-off-by: Henrik Steen <henrist@henrist.net>

--- include/uapi/linux/pkt_sched.h
+++ include/uapi/linux/pkt_sched.h
@@ -1160,4 +1160,36 @@ enum {
 
 #define TCA_TAPRIO_ATTR_MAX (__TCA_TAPRIO_ATTR_MAX - 1)
 
+/* DUALPI2 */
+enum {
+	TCA_DUALPI2_UNSPEC,
+	TCA_DUALPI2_ALPHA,
+	TCA_DUALPI2_BETA,
+	TCA_DUALPI2_DUALQ,
+	TCA_DUALPI2_ECN,
+	TCA_DUALPI2_K,
+	TCA_DUALPI2_L_DROP,
+	TCA_DUALPI2_ET_PACKETS,
+	TCA_DUALPI2_L_THRESH,
+	TCA_DUALPI2_LIMIT,
+	TCA_DUALPI2_T_SHIFT,
+	TCA_DUALPI2_T_SPEED,
+	TCA_DUALPI2_TARGET,
+	TCA_DUALPI2_TUPDATE,
+	TCA_DUALPI2_DROP_EARLY,
+	TCA_DUALPI2_WRR_RATIO,
+	__TCA_DUALPI2_MAX
+};
+
+#define TCA_DUALPI2_MAX   (__TCA_DUALPI2_MAX - 1)
+struct tc_dualpi2_xstats {
+	__u32 prob;             /* current probability */
+	__u32 delay_c;          /* current delay in C queue */
+	__u32 delay_l;          /* current delay in L queue */
+	__u32 packets_in;       /* total number of packets enqueued */
+	__u32 dropped;          /* packets dropped due to pie_action */
+	__u32 overlimit;        /* dropped due to lack of space in queue */
+	__u32 maxq;             /* maximum queue size */
+	__u32 ecn_mark;         /* packets marked with ecn*/
+};
 #endif
